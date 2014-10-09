2a7bd15fdad8289193e355d242c02ec1b4aa639e
Fabio Poloni <fabio@APP-roved.com>
2014-10-09 20:17:23 +0200

    Added Async.eachSync()

--- Async-Test/Async_Test.swift
+++ Async-Test/Async_Test.swift
@@ -65,4 +65,58 @@ class Async_Test: XCTestCase {
         self.waitForExpectationsWithTimeout(5.0, handler: nil)
     }
     
+    func testEachSync() {
+        var expectation = self.expectationWithDescription("Async.eachSync()")
+        
+        var items = ["Item 1", "Item 2"]
+        var iteratorCount = 0
+        var finishedCount = 0
+        
+        Async.eachSync(items, iterator: { (item, asyncCallback) -> Void in
+            
+            XCTAssertEqual(item, items[iteratorCount], "Item does not match")
+            XCTAssertLessThan(iteratorCount++, items.count, "Iterator was called too often")
+            
+            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) { () -> Void in
+                asyncCallback(error: nil)
+            }
+            
+        }) { (error: String?) -> Void in
+            
+            XCTAssertLessThan(finishedCount++, 1, "Finished was called too often")
+            
+            expectation.fulfill()
+        }
+        
+        self.waitForExpectationsWithTimeout(5.0, handler: nil)
+    }
+    
+    func testEachSyncWithError() {
+        var expectation = self.expectationWithDescription("Async.eachSync() with error")
+        
+        var items = ["Item 1", "Item 2"]
+        var expectedError = "This is an error"
+        var iteratorCount = 0
+        var finishedCount = 0
+        
+        Async.eachSync(items, iterator: { (item, asyncCallback) -> Void in
+            
+            XCTAssertEqual(item, items[iteratorCount], "Item does not match")
+            XCTAssertLessThan(iteratorCount++, items.count, "Iterator was called too often")
+            
+            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) { () -> Void in
+                asyncCallback(error: expectedError)
+            }
+            
+            }) { (error: String?) -> Void in
+                
+                XCTAssertLessThan(finishedCount++, 1, "Finished was called too often")
+                XCTAssertNotNil(error, "There was no error (and there should have been one)")
+                XCTAssertEqual(error!, expectedError, "Error does not match")
+                
+                expectation.fulfill()
+        }
+        
+        self.waitForExpectationsWithTimeout(5.0, handler: nil)
+    }
 }
--- Async.swift
+++ Async.swift
@@ -32,4 +32,36 @@ struct Async {
             }
         }
     }
+    
+    static func eachSync<ArrayType, ErrorType>(var arr: [ArrayType], iterator: (item: ArrayType, asyncCallback: (error: ErrorType?) -> Void) -> Void, finished: (error: ErrorType?) -> Void) {
+        
+        var isFinishedCalled = false
+        var finishedOnce = { (error: ErrorType?) -> Void in
+            if !isFinishedCalled {
+                isFinishedCalled = true
+                finished(error: error)
+            }
+        }
+        
+        var next: (() -> Void)?
+        
+        next = { () -> Void in
+            if arr.count > 0 {
+                let item = arr.removeAtIndex(0)
+                iterator(item: item) { (error) -> Void in
+                    if error != nil {
+                        finishedOnce(error)
+                    } else {
+                        next!()
+                    }
+                }
+            } else {
+                finishedOnce(nil)
+            }
+        }
+        
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) { () -> Void in
+            next!()
+        }
+    }
 }
